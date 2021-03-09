dbd3446fed4b0e47a03911dec57fc570e8b4c6b2
Dimitri Rusin <dimitri.rusin@smartfactory.de>
2021-03-09 17:39:02 +0100

    Simplify the program to download code from the internet.

--- code_from_keywords.py
+++ code_from_keywords.py
@@ -1,60 +1,53 @@
-from html.parser import HTMLParser
+import html.parser
 import requests
 import sys
 
-isResult = False
-isCode = False
+class SearchPageParser(html.parser.HTMLParser):
 
-webAddresses = []
-
-class MyHTMLParser(HTMLParser):
+    isResult = False
+    webAddresses = []
 
     def handle_starttag(self, tag, attrs):
-        global isResult
         if tag == 'a':
             if ('class', 'result__url') in attrs:
-                isResult = True
+                self.isResult = True
 
     def handle_endtag(self, tag):
-        global isResult
         if tag == 'a':
-            isResult = False
+            self.isResult = False
 
     def handle_data(self, data):
-        global isResult
-        if isResult:
-            webAddresses.append(data.strip())
+        if self.isResult:
+            self.webAddresses.append(data.strip())
+
+class CodePageParser(html.parser.HTMLParser):
+
+    isCode = False
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'code':
+            self.isCode = True
+
+    def handle_endtag(self, tag):
+        if tag == 'code':
+            self.isCode = False
+
+    def handle_data(self, data):
+        if self.isCode:
+            print(data)
+
 
 searchAddress = 'https://html.duckduckgo.com/html?q={}'.format("+".join(sys.argv[1:]))
-r = requests.get(searchAddress, headers={'user-agent': 'code_from_keywords/0.0.1'})
-parser = MyHTMLParser()
-parser.feed(r.text)
+searchPageParser = SearchPageParser()
+searchPageParser.feed(requests.get(searchAddress, headers={'user-agent': 'code_from_keywords/0.0.1'}).text)
 
 print("--------------------------------------------------")
 print(searchAddress)
 
-for webAddress in webAddresses:
+codePageParser = CodePageParser()
+for webAddress in searchPageParser.webAddresses:
 
     print("==================================================")
     print(webAddress)
 
-    class MySecondHTMLParser(HTMLParser):
-
-        def handle_starttag(self, tag, attrs):
-            global isCode
-            if tag == 'code':
-                isCode = True
-
-        def handle_endtag(self, tag):
-            global isCode
-            if tag == 'code':
-                isCode = False
-
-        def handle_data(self, data):
-            global isCode
-            if isCode:
-                print(data)
-
-    r = requests.get("https://" + webAddress)
-    parser = MySecondHTMLParser()
-    parser.feed(r.text)
+    codePageParser.feed(requests.get("https://" + webAddress).text)
