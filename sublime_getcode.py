90dd4bf04b46bf3ff79d724cba77e0a81787a8d6
Dimitri Rusin <dimitri.rusin@rwth-aachen.de>
2022-05-20 01:06:48 +0700

    Create a separate Linux process to download code from the Internet into a new file on the operating system; then open this file with Sublime Text, as it's getting populated.

--- sublime_getcode.py
+++ sublime_getcode.py
@@ -1,11 +1,10 @@
-import argparse
-import html.parser
-import queue
-import requests
+# Just run the getcode.py script, putting the results into a separate file, and open that file with Sublime Text
+# for easy viewing of valuable code from the Internet.
+
 import sublime
 import sublime_plugin
-import sys
-import threading
+import subprocess
+import tempfile
 
 # Put
 # { "keys": ["ctrl+shift+g"], "command": "getcode" },
@@ -23,86 +22,6 @@ import threading
 # Type Ctrl+Shift+P
 # Type Package Control: Satisfy Dependencies
 
-def getcode(codeQueue, keywords):
-
-  class SearchPageParser(html.parser.HTMLParser):
-
-    isResult = False
-    webAddresses = []
-
-    def handle_starttag(self, tag, attrs):
-      if tag == "a":
-        if ("class", "result__url") in attrs:
-          self.isResult = True
-
-    def handle_endtag(self, tag):
-      if tag == "a":
-        self.isResult = False
-
-    def handle_data(self, data):
-      if self.isResult:
-        self.webAddresses.append(data.strip())
-
-  class CodePageParser(html.parser.HTMLParser):
-
-    isCode = False
-
-    def handle_starttag(self, tag, attrs):
-      if tag in ["code"]:
-        self.isCode = True
-      if tag == "td":
-        attrsDict = dict(attrs)
-        if "class" in attrsDict:
-          classString = attrsDict["class"]
-          words = classString.split(" ")
-          if "blob-code" in words:
-            self.isCode = True
-      if codeQueue.maxsize > 0 and tag == "br" and self.isCode:
-        codeQueue.put("")
-        codeQueue.maxsize -= 1
-
-    def handle_endtag(self, tag):
-      if tag in ["code", "td"] and self.isCode:
-        self.isCode = False
-        if codeQueue.maxsize > 0:
-          codeQueue.put("")
-          codeQueue.maxsize -= 1
-
-    def handle_data(self, data):
-      if self.isCode:
-        data_lines = data.split("\n")
-        for data_line in data_lines:
-          if codeQueue.maxsize > 0:
-            codeQueue.put(data_line)
-            codeQueue.maxsize -= 1
-
-  searchAddress = "https://html.duckduckgo.com/html?q={}".format(keywords)
-  searchPageParser = SearchPageParser()
-  searchPageParser.feed(requests.get(searchAddress, headers={"user-agent": "getcode/0.0.1"}).text)
-
-  codeQueue.put("Fetching the first lines of Internet code described by \"{}\"".format(keywords))
-  codeQueue.put("--------------------------------------------------")
-  codeQueue.put(searchAddress)
-  codeQueue.maxsize -= 3
-
-  codePageParser = CodePageParser()
-  for webAddress in searchPageParser.webAddresses:
-
-    if codeQueue.maxsize == 0: break
-
-    codeQueue.put("==================================================")
-
-    webAddressWithProtocol = "http://" + webAddress
-    codeQueue.put(webAddressWithProtocol)
-    codeQueue.maxsize -= 2
-
-    try:
-      codePageParser.feed(requests.get(webAddressWithProtocol).text)
-    except requests.exceptions.ConnectionError as exception:
-      print("GETCODE ERROR requests.exceptions.ConnectionError: " + str(exception), file=sys.stderr)
-
-  codeQueue.put("STOP")
-
 class KeywordsInputHandler(sublime_plugin.TextInputHandler):
 
   def placeholder(self):
@@ -115,14 +34,18 @@ class GetcodeCommand(sublime_plugin.TextCommand):
 
   def run(self, edit, keywords):
 
-    codeQueue = queue.Queue(120)
+    # Erstelle selbst einen Dateinamen, welcher den Befehlstext enthält.
+    # Is keywords a list?
+    command_list = ['getcode', 'pandas rename column']
+    tempdir_path = tempfile.gettempdir()
+    zusammen = '_'.join(command_list[1:]).replace(' ', '_')
 
-    fetchingThread = threading.Thread(name="Page-Parser", target=getcode, args=(codeQueue, keywords,))
-    fetchingThread.start()
+    code_path = tempdir_path + '/' + zusammen
+
+    with open(code_path, 'w') as code_file:
+      proc = subprocess.Popen(['getcode', 'pandas rename column'], stdout=code_file, stderr=code_file)
 
     window = sublime.active_window()
-    fetching_view = window.new_file()
+    fetching_view = window.open_file(code_path)
+    # Set title of fetching_view with `keywords`
     window.focus_view(fetching_view)
-
-    for code_line in iter(codeQueue.get, "STOP"):
-      fetching_view.run_command("append", {"characters": code_line + "\n"})
